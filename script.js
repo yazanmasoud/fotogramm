cff9516ea1f4a6d77a2bb6cd88228a8ba4112bea
Yazan Masoud <yazanmas895@gmail.com>
2026-01-11 21:46:32 +0100

    Add new dog images and update image array in script.js; enhance style.css for improved layout and responsiveness

--- script.js
+++ script.js
@@ -1,27 +1,28 @@
 const images = [
     "./assets/img/Shiba_Inu.jpg",
-    "./assets/img/Border_Collie_Mix.jpg",
+    "./assets/img/Border.jpg",
     "./assets/img/Cockapoo.jpg",
-    "./assets/img/Dackel_Black_Red.jpg",
-    "./assets/img/Dackel_Short_Hair.jpg",
+    "./assets/img/Dackel1.jpg",
+    "./assets/img/Dackel3.jpg",
     "./assets/img/Dackel.jpg",
     "./assets/img/Dackel2.jpg",
-    "./assets/img/Golden_Retriever.jpg",
-    "./assets/img/Labrador_Retriever.jpg",
+    "./assets/img/Retriever.jpg",
+    "./assets/img/Labrador.jpg",
     "./assets/img/Mischling.jpg"
 
 
 ];
 
 const albumContainer = document.getElementById("photoAlbum");
+let currentIndex = 0;
+
 function rendPhotos(i) {
+    albumContainer.innerHTML = "";
     for (let i = 0; i < images.length; i++) {
         const image = images[i];
         albumContainer.innerHTML += `<img class="PhotoAlbumImage" src="${image}" tabindex="0" role="button" onclick="openDialog(${i})" onkeydown="if(event.key==='Enter'){openDialog(${i})}">`;
     }
 }
-
-let currentIndex = 0;
 
 function openDialog(index) {
     currentIndex = index;
@@ -35,7 +36,6 @@ function openDialog(index) {
 function closeDialog() {
     document.getElementById("dialogAlbum").close();
 }
-
 
 function showNextImage() {
     currentIndex = (currentIndex + 1) % images.length;
@@ -51,7 +51,6 @@ function showPrevImage() {
     updateDialogTitel()
 }
 
-
 /* this function shows the number of the photo in the dialog */
 function updateDialogImage() {
     const dialogImage = document.getElementById("dialogImage");
@@ -60,13 +59,13 @@ function updateDialogImage() {
     dialogImage.src = images[currentIndex];
     counter.innerHTML = `${currentIndex + 1} / ${images.length}`;
 }
+
 function updateDialogTitel() {
     const imagePath = images[currentIndex];
     const fileName = imagePath.split("/").pop();
     const titel = fileName.split(".")[0]; /* this is to switch the string to array and gives the first element back */
     const dialogTitel = document.getElementById('dialogTitel');
     dialogTitel.innerText = titel;
-
 }
 
 
