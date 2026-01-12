bc4d240fc89048f199dfa1654105c5d037c652d5
Yazan Masoud <yazanmas895@gmail.com>
2026-01-12 07:13:13 +0100

    Refactor variable names to uppercase for consistency; update image array references in script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const images = [
+const IMAGES = [
     "./assets/img/Shiba_Inu.jpg",
     "./assets/img/Border.jpg",
     "./assets/img/Cockapoo.jpg",
@@ -9,28 +9,26 @@ const images = [
     "./assets/img/Retriever.jpg",
     "./assets/img/Labrador.jpg",
     "./assets/img/Mischling.jpg"
-
-
 ];
 
-const albumContainer = document.getElementById("photoAlbum");
+const ALBUMCONTAINER = document.getElementById("photoAlbum");
 let currentIndex = 0;
 
 function rendPhotos(i) {
-    albumContainer.innerHTML = "";
-    for (let i = 0; i < images.length; i++) {
-        const image = images[i];
-        albumContainer.innerHTML += `<img class="PhotoAlbumImage" src="${image}" tabindex="0" role="button" onclick="openDialog(${i})" onkeydown="if(event.key==='Enter'){openDialog(${i})}">`;
+    ALBUMCONTAINER.innerHTML = "";
+    for (let i = 0; i < IMAGES.length; i++) {
+        const IMAGE = IMAGES[i];
+        ALBUMCONTAINER.innerHTML += `<img class="PhotoAlbumImage" src="${IMAGE}" tabindex="0" role="button" onclick="openDialog(${i})" onkeydown="if(event.key==='Enter'){openDialog(${i})}">`;
     }
 }
 
 function openDialog(index) {
     currentIndex = index;
-    const dialog = document.getElementById("dialogAlbum");
-    const dialogImage = document.getElementById("dialogImage");
+    const DIALOG = document.getElementById("dialogAlbum");
+    const DIALOGIMAGE = document.getElementById("dialogImage");
     updateDialogImage();
     updateDialogTitel()
-    dialog.showModal();
+    DIALOG.showModal();
 }
 
 function closeDialog() {
@@ -38,34 +36,31 @@ function closeDialog() {
 }
 
 function showNextImage() {
-    currentIndex = (currentIndex + 1) % images.length;
-    dialogImage.src = images[currentIndex];
+    currentIndex = (currentIndex + 1) % IMAGES.length;
     updateDialogImage()
     updateDialogTitel()
 }
 
 function showPrevImage() {
-    currentIndex = (currentIndex - 1 + images.length) % images.length;
-    dialogImage.src = images[currentIndex];
+    currentIndex = (currentIndex - 1 + IMAGES.length) % IMAGES.length;
     updateDialogImage()
     updateDialogTitel()
 }
 
 /* this function shows the number of the photo in the dialog */
 function updateDialogImage() {
-    const dialogImage = document.getElementById("dialogImage");
-    const counter = document.getElementById("currentImageNumber");
-
-    dialogImage.src = images[currentIndex];
-    counter.innerHTML = `${currentIndex + 1} / ${images.length}`;
+    const DIALOGIMAGE = document.getElementById("dialogImage");
+    const COUNTER = document.getElementById("currentImageNumber");
+    DIALOGIMAGE.src = IMAGES[currentIndex];
+    COUNTER.innerHTML = `${currentIndex + 1} / ${IMAGES.length}`;
 }
 
 function updateDialogTitel() {
-    const imagePath = images[currentIndex];
-    const fileName = imagePath.split("/").pop();
-    const titel = fileName.split(".")[0]; /* this is to switch the string to array and gives the first element back */
-    const dialogTitel = document.getElementById('dialogTitel');
-    dialogTitel.innerText = titel;
+    const IMAGEPATH = IMAGES[currentIndex];
+    const FILENAME = IMAGEPATH.split("/").pop();
+    const TITLE = FILENAME.split(".")[0]; /* this is to switch the string to array and gives the first element back */
+    const DIALOGTITLE = document.getElementById('dialogTitel');
+    DIALOGTITLE.innerText = TITLE;
 }
 
 
